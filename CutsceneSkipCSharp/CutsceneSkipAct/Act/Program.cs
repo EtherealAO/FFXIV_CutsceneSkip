ce4ceb30b16e14463eb83af91456c657f89bdf61
decades42 <83888870+decades42@users.noreply.github.com>
2021-09-12 02:46:04 +0800

    troubleshooting guide

--- CutsceneSkipCSharp/CutsceneSkipAct/Act/Program.cs
+++ CutsceneSkipCSharp/CutsceneSkipAct/Act/Program.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("CutsceneSkip")]
 [assembly: AssemblyDescription("Skip Cutscenes in MSQ Roulette")]
 [assembly: AssemblyCompany("Bluefissure, modified by winter")]
-[assembly: AssemblyVersion("1.0.2.1")]
+[assembly: AssemblyVersion("1.0.2.2")]
 [assembly: AssemblyCopyright("Copyright © Bluefissure 2021")]
 
 namespace CutsceneSkip
@@ -26,140 +26,150 @@ namespace CutsceneSkip
         public PluginEntry()
         { }
 
-		private Label m_lbPluginStats;
-		private Label m_lbPluginInfo;
-		private string m_lbStupidGameProcessInfo;
-		private BackgroundWorker m_bgProcessMoniotr;
-		private Process m_GameProcess;
-		private FFXIV_ACT_Plugin.FFXIV_ACT_Plugin m_ParsingPlugin;
+        private Label m_lbPluginStats;
+        private Label m_lbPluginInfo;
+        private string m_lbStupidGameProcessInfo;
+        private BackgroundWorker m_bgProcessMoniotr;
+        private Process m_GameProcess;
+        private FFXIV_ACT_Plugin.FFXIV_ACT_Plugin m_ParsingPlugin;
 
-		[DllImport("CutsceneSkipCore.dll")]
-		private static extern int initialize(int pid);
+        [DllImport("CutsceneSkipCore.dll")]
+        private static extern int initialize(int pid);
 
-		[DllImport("CutsceneSkipCore.dll")]
-		private static extern int destroy();
+        [DllImport("CutsceneSkipCore.dll")]
+        private static extern int destroy();
 
-		[DllImport("CutsceneSkipCore.dll")]
-		private static extern int on_read_log(string msg, string zone);
+        [DllImport("CutsceneSkipCore.dll")]
+        private static extern int on_read_log(string msg, string zone);
 
-		private void OnLogLineRead(bool bIsImport, LogLineEventArgs args)
+        private void OnLogLineRead(bool bIsImport, LogLineEventArgs args)
         {
-			int result = on_read_log(args.originalLogLine, args.detectedZone);
-			switch (result)
+            int result = on_read_log(args.originalLogLine, args.detectedZone);
+            switch (result)
             {
-				case 0:
-					m_lbPluginInfo.Text = "Disabled" + m_lbStupidGameProcessInfo;
-					break;
-				case 1:
-					m_lbPluginInfo.Text = "Enabled" + m_lbStupidGameProcessInfo;
-					break;
-				case 2:
-					m_lbPluginInfo.Text = "Disabled ( 初见 )" + m_lbStupidGameProcessInfo;
-					MessageBox.Show("队伍里有初见，插件已关闭。如果是自己人重开插件即可正常使用", "CutsceneSkip");
-					break;
-			} 
-		}
+                case 0:
+                    m_lbPluginInfo.Text = "Disabled" + m_lbStupidGameProcessInfo;
+                    break;
+                case 1:
+                    m_lbPluginInfo.Text = "Enabled" + m_lbStupidGameProcessInfo;
+                    break;
+                case 2:
+                    m_lbPluginInfo.Text = "Disabled ( 初见 )" + m_lbStupidGameProcessInfo;
+                    MessageBox.Show("队伍里有初见，插件已关闭。如果是自己人重开插件即可正常使用", "CutsceneSkip");
+                    break;
+            } 
+        }
 
-		private Process GetGameProcess()
+        private Process GetGameProcess()
         {
-			return m_ParsingPlugin.DataRepository.GetCurrentFFXIVProcess();
-		}
+            return m_ParsingPlugin.DataRepository.GetCurrentFFXIVProcess();
+        }
 
-		private void ProcessMonitor(object sender, DoWorkEventArgs args)
+        // 从鲶鱼精邮差那儿拿的
+        private void ProcessMonitor(object sender, DoWorkEventArgs args)
         { 
-			while (true)
+            while (true)
             {
-				if (m_bgProcessMoniotr.CancellationPending)
+                if (m_bgProcessMoniotr.CancellationPending)
                 {
-					args.Cancel = true;
-					break;
+                    args.Cancel = true;
+                    break;
                 }
 
-				if (m_GameProcess != GetGameProcess())
+                if (m_GameProcess != GetGameProcess())
                 {
-					if (m_GameProcess != null)
-						destroy();
+                    if (m_GameProcess != null)
+                        destroy();
 
-					m_GameProcess = GetGameProcess();
+                    m_GameProcess = GetGameProcess();
 
-					if (m_GameProcess != null)
+                    if (m_GameProcess != null)
                     {
-						int result = initialize(m_GameProcess.Id);
-						if (result != 1)
-						{
-							throw new Exception(GetErrorMessage(result));
-						}
-						m_lbStupidGameProcessInfo = "\nProcess ID: " + m_GameProcess.Id + " ( 如果不跳动画请检查Process ID是否与解析插件里的一致 )\n";
-						m_lbPluginInfo.Text = "Initialized" + m_lbStupidGameProcessInfo;
-					}
-				}
+                        int result = initialize(m_GameProcess.Id);
+                        if (result != 1)
+                        {
+                            m_lbPluginStats.Text = "Failed --> " + GetErrorMessage(result);
+                            throw new Exception(GetErrorMessage(result));
+                        }
+                        m_lbStupidGameProcessInfo = "\nProcess ID: " + m_GameProcess.Id + " ( 如果不跳动画请检查Process ID是否与解析插件里的一致 )\n";
+                        m_lbPluginInfo.Text = "Initialized" + m_lbStupidGameProcessInfo;
+                    }
+                }
             }
-		}
+        }
 
-		private string GetErrorMessage(int result)
+        private string GetErrorMessage(int result)
         {
-			switch (result)
-			{
-				case 0:
-					return "无效handle.";
+            switch (result)
+            {
+                case 0:
+                    return "无效handle.";
 
-				case -1:
-					return "过期了.";
+                case -1:
+                    return "重要的东西被摸你傻偷走了.";
 
-				case -2:
-					return "权限不够.";
-			}
+                case -2:
+                    return "权限不够.";
+            }
 
-			return "";
-		}
+            return "";
+        }
 
-		private FFXIV_ACT_Plugin.FFXIV_ACT_Plugin GetParsingPlugin()
-		{
-			FFXIV_ACT_Plugin.FFXIV_ACT_Plugin entry = null;
-			foreach (var actPluginData in ActGlobals.oFormActMain.ActPlugins)
-			{
-				if (actPluginData.pluginFile.Name.ToUpper().Contains("FFXIV_ACT_Plugin".ToUpper()) && actPluginData.lblPluginStatus.Text.ToUpper().Contains("FFXIV Plugin Started.".ToUpper()))
-				{
-					entry = (FFXIV_ACT_Plugin.FFXIV_ACT_Plugin)actPluginData.pluginObj;
-				}
-			}
-			return entry ?? throw new Exception("找不到解析插件FFXIV_ACT_Plugin，请确保插件有安装并启动");
-		}
+        private FFXIV_ACT_Plugin.FFXIV_ACT_Plugin GetParsingPlugin()
+        {
+            FFXIV_ACT_Plugin.FFXIV_ACT_Plugin entry = null;
+            foreach (var actPluginData in ActGlobals.oFormActMain.ActPlugins)
+            {
+                if (actPluginData.pluginFile.Name.ToUpper().Contains("FFXIV_ACT_Plugin".ToUpper()) && actPluginData.lblPluginStatus.Text.ToUpper().Contains("FFXIV Plugin Started.".ToUpper()))
+                {
+                    entry = (FFXIV_ACT_Plugin.FFXIV_ACT_Plugin)actPluginData.pluginObj;
+                }
+            }
+            return entry ?? throw new Exception("找不到解析插件FFXIV_ACT_Plugin，请确保插件有安装并启动");
+        }
 
-		public void InitPlugin(TabPage PluginTab, Label StatusText)
-		{
-			m_lbPluginStats = StatusText;   // Hand the status label's reference to our local var
+        public void InitPlugin(TabPage PluginTab, Label StatusText)
+        {
+            m_lbPluginStats = StatusText;   // Hand the status label's reference to our local var
 
-			m_ParsingPlugin = GetParsingPlugin();
+            m_ParsingPlugin = GetParsingPlugin();
 
-			// Fuck you C#
-			var PluginData = ActGlobals.oFormActMain.PluginGetSelfData(this);
-			Environment.SetEnvironmentVariable("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + PluginData.pluginFile.DirectoryName);
+            // Fuck you C#
+            var PluginData = ActGlobals.oFormActMain.PluginGetSelfData(this);
+            Environment.SetEnvironmentVariable("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + PluginData.pluginFile.DirectoryName);
 
-			m_bgProcessMoniotr = new BackgroundWorker { WorkerSupportsCancellation = true };
-			m_bgProcessMoniotr.DoWork += ProcessMonitor;
-			m_bgProcessMoniotr.RunWorkerAsync();
+            m_bgProcessMoniotr = new BackgroundWorker { WorkerSupportsCancellation = true };
+            m_bgProcessMoniotr.DoWork += ProcessMonitor;
+            m_bgProcessMoniotr.RunWorkerAsync();
 
-			m_lbPluginInfo = new Label();
-			m_lbPluginInfo.AutoSize = true;
-			ActGlobals.oFormActMain.OnLogLineRead += new LogLineEventDelegate(OnLogLineRead);
+            if (Process.GetProcessesByName("ffxiv_dx11").Length == 0)
+            {
+                throw new Exception("你要先开游戏再加载插件");
+            }
 
-			PluginTab.Text = "CutsceneSkip";
-			PluginTab.Controls.Add(m_lbPluginInfo);
+            m_lbPluginInfo = new Label
+            {
+                AutoSize = true,
+                Text = "如果你看到这段文字，那就说明插件加载失败。建议打开ACT日志，拉到最底端，反馈错误日志。\n如果出现「无法加载 DLL“CutsceneSkipCore.dll”: 找不到指定的模块。」这个错误时请确保：\n    1. 有安装 VC++2019 运行库 \n    2. CutsceneSkipCore.dll 与 CutsceneSkip.dll 在同一目录下"
+            };
+            ActGlobals.oFormActMain.OnLogLineRead += new LogLineEventDelegate(OnLogLineRead);
 
-			m_lbPluginStats.Text = "Working";
+            PluginTab.Text = "CutsceneSkip";
+            PluginTab.Controls.Add(m_lbPluginInfo);
 
-			MessageBox.Show("仅在队伍中没有初见时才会工作。\n免费插件请勿倒卖。\n如果有初见且是自己人时重开插件就能正常使用", "CutsceneSkip");
-		}
+            m_lbPluginStats.Text = "Working";
 
-		public void DeInitPlugin()
-		{
-			ActGlobals.oFormActMain.OnLogLineRead -= OnLogLineRead;
-			m_bgProcessMoniotr.CancelAsync();
+            MessageBox.Show("仅在队伍中没有初见时才会工作。\n免费插件请勿倒卖。\n如果有初见且是自己人时重开插件就能正常使用", "CutsceneSkip");
+        }
 
-			int result = destroy();
+        public void DeInitPlugin()
+        {
+            ActGlobals.oFormActMain.OnLogLineRead -= OnLogLineRead;
+            m_bgProcessMoniotr.CancelAsync();
 
-			m_lbPluginStats.Text = "Exited\n" + GetErrorMessage(result);
-		}
-	}
+            int result = destroy();
+
+            m_lbPluginStats.Text = "Exited\n" + GetErrorMessage(result);
+        }
+    }
 }
